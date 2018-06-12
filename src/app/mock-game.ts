cc494934043b1f34f3c8bcf55a6a46db8863547a
Vanessa DAVID <vanessa.david@alten.com>
2018-06-12 11:38:13 +0200

    Adding details about a game

---
+++ src/app/mock-game.ts
@@ -0,0 +1,22 @@
+import { Game } from './game';
+
+export const GAMES: Game[] = [
+    {
+        name : 'Monopoly',
+        editor : 'Hasbro',
+        year_published : '2000',
+        description: 'Want to make money ? This a the game for you',
+        category : 'family',
+        time : '60 min',
+        number_player : '2-6'
+      },
+      {
+        name : 'Trivial pursuit',
+        editor : 'Hasbro',
+        year_published : '1980',
+        description: 'Want to make money ? This a the game for you',
+        category : 'family',
+        time : '60 min',
+        number_player : '2-6'
+      }
+];
